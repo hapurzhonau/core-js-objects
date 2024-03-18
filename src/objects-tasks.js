d86f80db134f67d4c82b91d62e9811542f91af08
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-03-18 16:34:42 +0300

    feat: complete 2nd task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,17 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, el) => {
+    Object.entries(el).forEach(([key]) => {
+      if (acc[key]) {
+        acc[key] += el[key];
+      } else {
+        acc[key] = el[key];
+      }
+    });
+    return acc;
+  }, {});
 }
 
 /**
